340b456fd77d5f13db842f57674b12926619155e
girish <girishproject28@gmail.com>
2026-01-02 12:14:39 +0000

    update2

---
+++ audio/manifest.js
@@ -0,0 +1,24 @@
+window.MANIFEST_RAW = [
+  {
+    id: "utt_0001",
+    gt_id: "utt_0001",
+    codec: "SpeechTokenizer",
+    lang: "hi",
+    split: "test",
+    protocol: "seen",
+    speaker_id: "spk_001",
+    gt_url: "audio/real/hi/utt_0001.wav",
+    gen_url: "audio/generated/hi/SpeechTokenizer/utt_0001.wav"
+  },
+  {
+    id: "utt_0001",
+    gt_id: "utt_0001",
+    codec: "SpeechTokenizer",
+    lang: "hi",
+    split: "test",
+    protocol: "seen",
+    speaker_id: "spk_001",
+    gt_url: "audio/real/hi/utt_0001.wav",
+    gen_url: "audio/generated/hi/SpeechTokenizer/utt_0001.wav"
+  }
+];
